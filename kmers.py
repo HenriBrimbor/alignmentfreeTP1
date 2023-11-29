a3bd82a8ba115738d4c6a732738df75ec6ba1529
HenriG <51200524+HenriBrimbor@users.noreply.github.com>
2023-11-29 22:15:23 +0100

    Test_fork_1

--- kmers.py
+++ kmers.py
@@ -16,5 +16,38 @@ def kmer2str(val, k):
 
 
 def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+    letters = ['A', 'C', 'T', 'G']
+    mask = (1 << (2*k))-1 # décallage de 2 bits fois k nucléotides (pour k-mer codés en 2-bits)
+    # pour k=2 encodage de 2 bits: 1<<4 -1 = 000001<<4 -1 = 010000 -1 = 001111
+    # l'opérateur & ET logique va donc filtrer les 4 derniers bits uniquement donc les deux derniers nucléotides
+
+    # initialisation des entiers, à voir comme des cases mémoire avec les bits
+    kmer = 0
+    comp = 0
+    retenue = 0
+    for index, letter in enumerate(text):
+        if letter in letters:
+            nucl = letters.index(letter)   # on cherche l'index de la lettre dans la liste, qui correspond à l'encodage 0,1,2,3 équivalent en binaire (0,0) (0,1) (1,0) (1,1)
+            kmer <<= 2                     # on décale les bits vers la gauche par rapport au cadre de lecture
+            kmer += nucl                   # on ajoute l'index composé de deux bits après le décalage donc sur la droite des précédents
+            kmer &= mask                   # on applique le masque pour ne prendre que les k derniers caractères (prise en compte des k*2 derniers bits)
+
+            # On fait la même chose avec le kmer complémentaire
+            nucl_comp = (nucl+2) % 4
+            if index+1-retenue < k:
+                comp += nucl_comp<<(2*(index-retenue+1))
+            # pour les k-1 premiers il ne faut rien renvoyer, car le masque prend les 0 à gauche en mémoire comme des A, alors qu'ils n'existent pas dans la séquence
+            else: # index-retenue+1 >= k:
+                comp += nucl_comp<<(2*(k))
+                comp >>= 2
+                yield kmer, comp
+        else:
+            # Dans le cas où l'on aurait une lettre qui n'est pas dans la liste des lettres, dans les k premières lettres
+            # on doit l'ignorer mais décrémenter l'index
+            if index+1 < k:
+                retenue += 1
+
+if __name__ == "__main__":
+    k = 3
+    for i in stream_kmers('AACACTG', k):
+        print( kmer2str(i[0], k), kmer2str(i[1], k) )
--- main.py
+++ main.py
@@ -1,25 +1,67 @@
 from loading import load_directory
 from kmers import stream_kmers, kmer2str
+import numpy as np
+import time
+import sys
 
-
+code = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
 
 def jaccard(fileA, fileB, k):
-    j = 0
-    # --- To complete ---
-    return j
+    """
+    fileA / fileB : lists of lists containing sequences on different chromosomes in A or B
+    k : integer for the length of kmers
+    """
+    kmers_A = {}
+    for list_Ai in fileA:
+        for kmer,kmer_comp in stream_kmers(list_Ai, k):
+            kmers_A[kmer]      = kmers_A.get(kmer, 0) + 1
+            kmers_A[kmer_comp] = kmers_A.get(kmer_comp, 0) + 1
+            
+    kmers_B = {}
+    for list_Bi in fileB:
+        for kmer,kmer_comp in stream_kmers(list_Bi, k):
+            kmers_B[kmer]      = kmers_B.get(kmer, 0) + 1
+            kmers_B[kmer_comp] = kmers_B.get(kmer_comp, 0) + 1
+
+    intersect = 0
+    for key in kmers_A:
+        intersect += min( kmers_A[key], kmers_B.get(key,0) )
+
+    #union = sum(kmers_A.values()) + sum(kmers_B.values()) 
+    union = 0
+    for key in kmers_A:
+        union += max( kmers_A[key], kmers_B.get(key,0) )
+    for key in kmers_B:
+        union += max( kmers_B[key], kmers_A.get(key,0) )
+    # A.U.B = A+B-(A/-\B)
+    union -= intersect
+    
+    size = sys.getsizeof(kmers_A) + sys.getsizeof(kmers_B)
+    del kmers_A
+    del kmers_B
+    
+    return intersect / union, size
 
 
 
 if __name__ == "__main__":
     # Load all the files in a dictionary
+    start = time.time()
     files = load_directory("data")
     k = 21
-    
+    l = len(files)
+    sizes = []
+    matrix = np.zeros((l,l))
     filenames = list(files.keys())
-    for i in range(len(files)):
-        for j in range(i+1, len(files)):
-            
-            # --- Complete here ---
-
-            j = jaccard(files[filenames[i]], files[filenames[j]], k)
-            print(filenames[i], filenames[j], j)
+    for i in range(l):
+        for j in range(i+1, l):
+            jacc,size = jaccard(files[filenames[i]], files[filenames[j]], k)
+            matrix[i,j]  = jacc
+            sizes.append(size)
+            print(filenames[i], filenames[j], jacc)
+    print(matrix)
+    print(sizes)
+    end = time.time()
+    print('Temps écoulé : {}'.format(end-start))
+    # Temps moyen : 260s à améliorer
+    

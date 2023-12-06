1d1e9efef18e0f3c1360d9a584c0814578a12b9a
HenriG <51200524+HenriBrimbor@users.noreply.github.com>
2023-12-07 00:24:49 +0100

    TP1_kmers_encodage
    Amélioration du premier fork, augmentation de l'utilisation mémoire pour stocker les kmers et éviter de les recalculer, libération mémoire à chaque tour de boucle

--- README.md
+++ README.md
@@ -14,3 +14,16 @@ Vous devez compléter le README pour y afficher la matrice des distances des bac
 Vous devez également y indiquer le temps d'exécution qu'il a fallu pour calculer cette matrice ainsi que l'espace mémoire maximale. Pour cela vous pouvez utiliser la commande ```/usr/bin/time -v```.
 
 En observant les distances obtenues, que pouvez-vous dire des espèces présentes dans cet échantillon ?
+
+La commande ci-dessus ne renvoyait environ 3 minutes mais aucune mémoire utilisée.
+
+0 GCF_000006945.2_ASM694v2_genomic.fna
+1 GCF_008244785.1_ASM824478v1_genomic.fna
+2 GCF_014892695.1_ASM1489269v1_genomic.fna
+3 GCF_020526745.1_ASM2052674v1_genomic.fna
+4 GCF_020535205.1_ASM2053520v1_genomic.fna
+0.0	0.9305327893323498	0.001758029858018395	0.019080168523989725	0.01790100536745689
+0.0	0.0	0.0017668568443008623	0.01931427581093962	0.01800350125732436
+0.0	0.0	0.0	0.0019422313931300474	0.003889458143281781
+0.0	0.0	0.0	0.0	0.606311315482450
+0.0	0.0	0.0	0.0	0.
--- main.py
+++ main.py
@@ -6,22 +6,12 @@ import sys
 
 code = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
 
-def jaccard(fileA, fileB, k):
+
+def jaccard(kmers_A, kmers_B):
     """
     fileA / fileB : lists of lists containing sequences on different chromosomes in A or B
     k : integer for the length of kmers
     """
-    kmers_A = {}
-    for list_Ai in fileA:
-        for kmer,kmer_comp in stream_kmers(list_Ai, k):
-            kmers_A[kmer]      = kmers_A.get(kmer, 0) + 1
-            kmers_A[kmer_comp] = kmers_A.get(kmer_comp, 0) + 1
-            
-    kmers_B = {}
-    for list_Bi in fileB:
-        for kmer,kmer_comp in stream_kmers(list_Bi, k):
-            kmers_B[kmer]      = kmers_B.get(kmer, 0) + 1
-            kmers_B[kmer_comp] = kmers_B.get(kmer_comp, 0) + 1
 
     intersect = 0
     for key in kmers_A:
@@ -36,32 +26,77 @@ def jaccard(fileA, fileB, k):
     # A.U.B = A+B-(A/-\B)
     union -= intersect
     
-    size = sys.getsizeof(kmers_A) + sys.getsizeof(kmers_B)
     del kmers_A
     del kmers_B
     
-    return intersect / union, size
+    return intersect / union
 
 
 
 if __name__ == "__main__":
-    # Load all the files in a dictionary
     start = time.time()
-    files = load_directory("data")
-    k = 21
+    
+    
+    files = load_directory("data") # Load all the files in a dictionary
+    k = 21                         # setting the length of kmers
     l = len(files)
-    sizes = []
-    matrix = np.zeros((l,l))
-    filenames = list(files.keys())
+    matrix       = np.zeros((l,l))
+    filenames    = list(files.keys())
+    kmers_files  = {}
+    max_memory   = 0
+    files_memory = np.sum([np.sum([sys.getsizeof(i) for i in file]) for file in files]) 
+
+    # Rather than calculating every kmers of the same file at each iteration
+    # we store them in a dictionnary outside of jaccard to calculate them only once
     for i in range(l):
+        # if the kmers of file i are met for the first time build them
+        if i not in kmers_files:
+            kmers_files[i] = {}
+            for list_Ai in files[filenames[i]]:
+                for kmer, kmer_comp in stream_kmers(list_Ai, k):
+                    kmers_files[i][kmer]      = kmers_files[i].get(kmer, 0) + 1
+                    kmers_files[i][kmer_comp] = kmers_files[i].get(kmer_comp, 0) + 1
+            
         for j in range(i+1, l):
-            jacc,size = jaccard(files[filenames[i]], files[filenames[j]], k)
-            matrix[i,j]  = jacc
-            sizes.append(size)
-            print(filenames[i], filenames[j], jacc)
+            # if the kmers of file j are met for the first time build them
+            if j not in kmers_files:
+                    kmers_files[j] = {}
+                    for list_Bi in files[filenames[j]]:
+                        for kmer, kmer_comp in stream_kmers(list_Bi, k):
+                            kmers_files[j][kmer]      = kmers_files[j].get(kmer, 0) + 1
+                            kmers_files[j][kmer_comp] = kmers_files[j].get(kmer_comp, 0) + 1
+                            
+            matrix[i,j] = jaccard( kmers_files[i], kmers_files[j] )
+            #print(filenames[i], filenames[j], matrix[i,j])
+
+        max_memory = max(max_memory, np.sum( [sys.getsizeof(kmers_files[i]) for i in kmers_files.keys()]) )
+        # All calculations done for file i, releasing the memory
+        del kmers_files[i]
+        
     print(matrix)
-    print(sizes)
     end = time.time()
-    print('Temps écoulé : {}'.format(end-start))
-    # Temps moyen : 260s à améliorer
+    
+    # Amélioration : from 280s to 180s but uses more memory
+    print('Time spent : {}'.format( end-start) )
+    print('Max memory used with dict : {}'.format ( max_memory  ) )
+    print('Max memory used with files : {}'.format( files_memory) )
+    print('Sum : {} kb = {} Tb'.format( files_memory+max_memory, round((files_memory+max_memory)/10**9, 4) ) )
+    
+    with open('./README.md', 'w') as f:
+        for index,i in enumerate(files.keys()):
+            f.write( '#{} {}\n'.format(index, i))
+        for line in matrix:
+            string = ""
+            for item in line:
+                string += str(item) + '\t'
+            string = string[:-2]+'\n'
+            f.write(string)
+        
+    
+    
+    
+    
+    
+    
+    
     
